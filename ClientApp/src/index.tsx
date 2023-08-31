4dc33d5837e65ca0721c6152609fd3d88d79edfc
irsat000 <irsat000@gmail.com>
2023-08-31 21:26:28 +0300

    Fixed scroll down at render with ScrollToTop

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -5,12 +5,14 @@ import './styles/global.css';
 import './styles/header.css';
 import './styles/footer.css';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <BrowserRouter basename={baseUrl ?? undefined}>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
