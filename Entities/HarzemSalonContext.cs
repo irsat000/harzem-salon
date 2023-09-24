25d7839f562f3aa96db5bea3d77cc29071430723
irsat000 <irsat000@gmail.com>
2023-09-24 14:20:53 +0300

    Connection string as user-secret

--- Entities/HarzemSalonContext.cs
+++ Entities/HarzemSalonContext.cs
@@ -30,8 +30,7 @@ public partial class HarzemSalonContext : DbContext
     public virtual DbSet<Testimonial> Testimonials { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=HarzemSalon;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Name=HarzemSalon:ConnectionString");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
